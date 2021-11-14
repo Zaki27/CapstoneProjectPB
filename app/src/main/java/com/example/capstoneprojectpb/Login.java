6a1ce370cd1a01be0e81d4027e08a6a39409808b
Muhamad Aziz Antabari Acta <Azizantabariacta30@gmail.com>
2021-11-15 02:23:49 +0700

    halaman Register

--- app/src/main/java/com/example/capstoneprojectpb/Login.java
+++ app/src/main/java/com/example/capstoneprojectpb/Login.java
@@ -10,18 +10,28 @@ import androidx.cardview.widget.CardView;
 
 public class Login extends AppCompatActivity {
     CardView cardView;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         cardView = findViewById(R.id.cardView);
+        textView2 = findViewById(R.id.textView2);
 
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mainActivity = new Intent(Login.this, MainActivity.class);
                 startActivity(mainActivity);
+            }
+        });
+
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerActivity = new Intent(Login.this, Register.class);
+                startActivity(registerActivity);
             }
         });
     }
