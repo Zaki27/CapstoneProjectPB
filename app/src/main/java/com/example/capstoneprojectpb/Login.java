8848132fb638a279aec04a15556633b8af2fc7f0
muhammadrozan <rozanazzikrii@gmail.com>
2021-11-22 16:19:16 +0700

    Memperbaiki Halaman Login dan Register

--- app/src/main/java/com/example/capstoneprojectpb/Login.java
+++ app/src/main/java/com/example/capstoneprojectpb/Login.java
@@ -1,38 +1,83 @@
 package com.example.capstoneprojectpb;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build;
+
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 public class Login extends AppCompatActivity {
-    CardView cardView;
-    TextView textView2;
+
+    EditText TxUsername, TxPassword;
+    Button BtnLogin;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        cardView = findViewById(R.id.cardView);
-        textView2 = findViewById(R.id.textView2);
 
-        cardView.setOnClickListener(new View.OnClickListener() {
+        TxUsername = (EditText)findViewById(R.id.txUsername);
+        TxPassword = (EditText)findViewById(R.id.txPassword);
+        BtnLogin = (Button)findViewById(R.id.btnLogin);
+
+        dbHelper = new DBHelper(this);
+
+        TextView tvCreateAccount = (TextView)findViewById(R.id.tvCreateAccount);
+
+        tvCreateAccount.setText(fromHtml("Belum memiliki akun? "));
+        tvCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent mainActivity = new Intent(Login.this, MainActivity.class);
-                startActivity(mainActivity);
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
             }
         });
 
-        textView2.setOnClickListener(new View.OnClickListener() {
+        TextView tvCreateAccount2 = (TextView)findViewById(R.id.tvCreateAccount2);
+
+        tvCreateAccount2.setText(fromHtml("</font><font color='#3b5998'>Daftar sekarang!</font>"));
+        tvCreateAccount2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent registerActivity = new Intent(Login.this, Register.class);
-                startActivity(registerActivity);
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = TxUsername.getText().toString().trim();
+                String password = TxPassword.getText().toString().trim();
+
+                Boolean res = dbHelper.checkUser(username,password);
+                if(res == true){
+                    Toast.makeText(Login.this, "Login Berhasil!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                }else {
+                    Toast.makeText(Login.this, "Login Gagal!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+    public static Spanned fromHtml(String html){
+        Spanned result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        }else {
+            result = Html.fromHtml(html);
+        }
+        return result;
+    }
 }
+
