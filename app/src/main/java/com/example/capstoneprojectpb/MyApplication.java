9d94b65900e42b91302fd4154b5ed2c857816c83
Muhamad Aziz Antabari Acta <Azizantabariacta30@gmail.com>
2021-11-25 02:54:02 +0700

    notification

---
+++ app/src/main/java/com/example/capstoneprojectpb/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.capstoneprojectpb;
+import android.app.Application;
+import com.google.firebase.messaging.FirebaseMessaging;
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseMessaging.getInstance().subscribeToTopic("sample_notification");
+
+    }
+}
