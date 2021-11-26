376797e3410b5ff83844c3d748d89f087b5debe2
rizkiade18 <rizkyadeningsih18@gmail.com>
2021-11-26 17:28:11 +0700

    Membuat halaman Versi Aplikasi

--- app/src/main/java/com/example/capstoneprojectpb/AppVersionActivity.java
+++ app/src/main/java/com/example/capstoneprojectpb/AppVersionActivity.java
@@ -1,6 +1,12 @@
 package com.example.capstoneprojectpb;
 
+import static android.text.Html.fromHtml;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +16,21 @@ public class AppVersionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_version);
+
+        TextView Contact = (TextView)findViewById(R.id.Contact);
+
+        Contact.setText(fromHtml("</font><font color='#3b5998'>CONTACT US</font>"));
+
+        Contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setData(Uri.parse("mailto:"));
+                email.setType("message/cfc822");
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"rizkyadeningsih18@gmail.com"});
+
+                startActivity(Intent.createChooser(email, "Send Mail"));
+            }
+        });
     }
 }
