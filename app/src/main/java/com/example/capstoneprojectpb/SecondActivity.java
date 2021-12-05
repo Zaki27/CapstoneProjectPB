b9f4f70912900b3e5d4c2c9a7a6d26771fddab15
muhammadrozan <rozanazzikrii@gmail.com>
2021-12-05 11:22:00 +0700

    Mengganti Logo pada beberapa halaman, dan icon launcher

--- app/src/main/java/com/example/capstoneprojectpb/SecondActivity.java
+++ app/src/main/java/com/example/capstoneprojectpb/SecondActivity.java
@@ -1,7 +1,5 @@
 package com.example.capstoneprojectpb;
 
-
-
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
@@ -25,8 +23,8 @@ import com.example.capstoneprojectpb.model.Result;
 
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_MOVIE = "string_extra" ;
-    ImageView second_back_arrow, second_arrow_up, imgFilmDetail;
-    TextView second_title, second_subtitle, second_rating_number, second_rating_number2, more_details;
+    ImageView second_back_arrow, imgFilmDetail;
+    TextView second_title, second_subtitle, second_rating_number, second_rating_number2;
     RatingBar second_ratingbar;
     Animation from_left, from_right, from_bottom;
     Result result;
@@ -40,16 +38,12 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         second_back_arrow = findViewById(R.id.second_back_arrow);
-        second_arrow_up = findViewById(R.id.seconf_arrow_up);
         second_title = findViewById(R.id.second_title);
         second_subtitle = findViewById(R.id.second_subtitle);
         second_rating_number = findViewById(R.id.second_rating_number);
         second_rating_number2 = findViewById(R.id.second_rating_number2);
-        more_details = findViewById(R.id.more_details);
         imgFilmDetail = findViewById(R.id.imgFilmDetail);
         second_ratingbar = findViewById(R.id.second_ratingbar);
-
-
 
         result = getIntent().getParcelableExtra(EXTRA_MOVIE);
 
@@ -61,13 +55,11 @@ public class SecondActivity extends AppCompatActivity {
         rilisFilm = result.getReleaseDate();
         story = result.getOverview();
 
-
-        //tarok data api di aplikasi
+        //masukkan data api di aplikasi
         second_rating_number.setText(nilai + "/10");
         second_title.setText(title);
         second_subtitle.setText(story);
         second_rating_number2.setText(rilisFilm);
-
 
         //rating film dalam bentuk bintang
         float newValue = (float)nilai;
@@ -86,6 +78,7 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         //Hide status bar and navigation bar at the bottom
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
@@ -110,18 +103,5 @@ public class SecondActivity extends AppCompatActivity {
         second_ratingbar.setAnimation(from_left);
         second_rating_number.setAnimation(from_right);
         second_rating_number2.setAnimation(from_right);
-        second_arrow_up.setAnimation(from_bottom);
-        more_details.setAnimation(from_bottom);
-        second_arrow_up.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
-                Pair[] pairs = new Pair[1];
-                pairs[0] = new Pair<View, String>(second_arrow_up, "background_image_transition");
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SecondActivity.this, pairs);
-                startActivity(intent, options.toBundle());
-            }
-        });
     }
 }
